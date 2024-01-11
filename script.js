09584db0c669b2c7be1f2668d68bede093b16672
Iulian Gabriel <burciuiuliangabriel18@gmail.com>
2024-01-11 10:44:44 +0200

    Subtle changes to html and css, added toggle grid button functionality

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const gridContainer = document.querySelector(".grid-container");
 const gridSizeSlider = document.querySelector(".grid-size-slider");
 const gridSizeText = document.querySelector(".grid-size-text");
+const toggleGridButton = document.querySelector(".grid-btn");
 
 gridSizeSlider.addEventListener("input", createGrid);
+toggleGridButton.addEventListener("click", toggleGrid);
 
 // Create and update grid on page.
 function createGrid() {
@@ -16,7 +18,7 @@ function createGrid() {
   for (let i = 0; i < defaultSize; i++) {
     for (let j = 0; j < defaultSize; j++) {
       const newDiv = document.createElement("div");
-      newDiv.className = "grid-cell hidden";
+      newDiv.className = "grid-cell";
       newDiv.style.width = `${wrapperMeasurements / defaultSize}px`;
       newDiv.style.height = `${wrapperMeasurements / defaultSize}px`;
       gridContainer.appendChild(newDiv);
@@ -25,3 +27,15 @@ function createGrid() {
 }
 
 createGrid();
+
+function toggleGrid() {
+  if(gridContainer.classList.contains('hidden')){
+    gridContainer.classList.remove('hidden')
+    toggleGridButton.style.backgroundColor = "#7f00ff"
+    toggleGridButton.style.color = "#202020"
+  } else {
+    gridContainer.classList.add('hidden')
+    toggleGridButton.style.backgroundColor = ""
+    toggleGridButton.style.color = ""
+  }
+}
